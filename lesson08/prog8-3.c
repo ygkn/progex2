8a19ff64c1ac9d831433bdfb375ffb7ce19f9244
Yugo Yagita <2000ygkn0713@gmail.com>
2020-07-06 14:13:12 +0900

    add test of selection_sort

--- lesson08/prog8-3.c
+++ lesson08/prog8-3.c
@@ -28,10 +28,35 @@ void selection_sort(struct data data[], int num) {
 
 /*=============================================*/
 
-void test1() {}
+void test1() {
+  struct data data[] = {{3, 'c'}, {1, 'a'}, {3, 'd'}, {2, 'b'}};
+  int num = sizeof(data) / sizeof(data[0]);
+
+  print(data, num);
+  selection_sort(data, num);
+  print(data, num);
+
+  assert(is_sorted(data, num));
+
+  printf("Success: %s\n", __func__);
+}
+
+void test2() {
+  struct data data[] = {{3, 'c'}, {2, 'b'}, {3, 'd'}, {1, 'a'}};
+  int num = sizeof(data) / sizeof(data[0]);
+
+  print(data, num);
+  selection_sort(data, num);
+  print(data, num);
+
+  assert(is_sorted(data, num));
+
+  printf("Success: %s\n", __func__);
+}
 
 int main() {
   test1();
+  test2();
 
   return 0;
 }
