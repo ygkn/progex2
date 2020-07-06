f9dc3755a24795476d5148230c3dd1d478cac688
Yugo Yagita <2000ygkn0713@gmail.com>
2020-07-06 14:22:35 +0900

    add test of insertion_sort

--- lesson08/prog8-4.c
+++ lesson08/prog8-4.c
@@ -25,7 +25,18 @@ void insertion_sort(struct data data[], int num) {
 
 /*=============================================*/
 
-void test1() {}
+void test1() {
+  struct data data[] = {{3, 'c'}, {1, 'a'}, {3, 'd'}, {2, 'b'}};
+  int num = sizeof(data) / sizeof(data[0]);
+
+  print(data, num);
+  insertion_sort(data, num);
+  print(data, num);
+
+  assert(is_sorted(data, num));
+
+  printf("Success: %s\n", __func__);
+}
 
 int main() {
   test1();
